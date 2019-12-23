204862f3b7233f96a2e5daeae7aeb8c29579d29d
Esa Väntsi <>
2019-12-23 14:39:19 +0200

    1.2

--- src/advent_of_code_2019/core.clj
+++ src/advent_of_code_2019/core.clj
@@ -6,11 +6,20 @@
                       int
                       (- 2)))
 
-
 (defn get-lines [filename] (map
                             #(Integer/parseInt %)
                             (str/split-lines
                              (slurp filename))))
 
+; 1.1
 (reduce + (map fuel (get-lines "resources/input.txt")))
 
+(defn total-fuel [mass]
+  (->> mass
+       (iterate fuel)
+       (take-while pos?)
+       rest
+       (reduce +)))
+
+; 1.2
+(reduce + (map total-fuel (get-lines "resources/input.txt")))
